061acc3b2ba3710db2e9067fb5fb3171d8425c43
Abhi <satpute.abhi22@gmail.com>
2026-01-26 12:43:32 +0530

    changes

--- app/src/main/java/com/roaa/docscanner/MainActivity.kt
+++ app/src/main/java/com/roaa/docscanner/MainActivity.kt
@@ -18,8 +18,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -29,7 +31,10 @@ import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
 import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 import com.roaa.docscanner.ui.theme.DocScannerTheme
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,12 +61,23 @@ class MainActivity : ComponentActivity() {
                             .padding(innerPadding),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        val imageUris = remember { mutableStateListOf<Uri>() }
+                        var imageUris by remember { mutableStateOf<List<Uri>>(emptyList()) }
 
                         val scannerLauncher = rememberLauncherForActivityResult(
                             contract = ActivityResultContracts.StartIntentSenderForResult(),
-                            onResult = { result ->
+                            onResult = {
                                 // Handle the result
+                                if (it.resultCode == RESULT_OK) {
+                                    val result = GmsDocumentScanningResult.fromActivityResultIntent(it.data)
+                                    imageUris = result?.pages?.map { it.imageUri } ?: emptyList()
+
+                                    result?.pdf?.let { pdf ->
+                                        val fos = FileOutputStream(File(filesDir,"scan.pdf"))
+                                        contentResolver.openInputStream(pdf.uri)?.use {
+                                            it.copyTo(fos)
+                                        }
+                                    }
+                                }
                             }
                         )
 
